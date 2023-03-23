43f28dfa35334227bf6f393e077f4d79665d30a7
mipekli <125420687+mipekli@users.noreply.github.com>
2023-03-23 16:04:28 +0300

    Add project files.

---
+++ csharp-enum/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine(gunler.Pazar);
+Console.WriteLine((int)gunler.Cumartesi);
+int sıcaklık = 32;
+if (sıcaklık <= (int)HavaDurumu.Normal)
+{
+     Console.WriteLine("Dışarıya çıkmak için havanın biraz daha ısınmasını bekle");
+
+} else if (sıcaklık>=(int)HavaDurumu.Sıcak)
+    
+{
+ Console.WriteLine("Dışarıy çıkmak için sıcak bir gün ");   
+}else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık < (int)HavaDurumu.Coksicak)
+{
+    Console.WriteLine("Hadi dışarıya çıkalım!");
+}
+enum gunler
+{
+    Pazartesi,
+    Salı,
+    Çarşamba,
+    Perşembe,
+    Cuma,
+    Cumartesi,
+    Pazar, 
+}
+
+enum HavaDurumu
+{
+    Soguk=5,
+    Normal=20,
+    Sıcak=25,
+    Coksicak=30,
+}
